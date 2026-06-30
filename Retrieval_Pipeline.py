776fa543750c87e6a7276d9e1164704b1337fa27
Nidula Nuran <it24102710@my.sliit.lk>
2026-06-30 14:25:42 +0530

    feat:Add chat history for retrieval pipeline

--- Retrieval_Pipeline.py
+++ Retrieval_Pipeline.py
@@ -1,56 +1,84 @@
-from chromadb.segment.impl.vector.hnsw_params import persistent_param_validators
-from langchain_classic.chains.question_answering.map_reduce_prompt import messages
 from langchain_huggingface import  HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
-from torch.nn.functional import embedding
 from langchain_groq import ChatGroq
-from langchain_core.messages import SystemMessage,HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
-embedding_model=HuggingFaceEmbeddings(model="all-MiniLM-L6-v2")
+embedding_model = HuggingFaceEmbeddings(model="all-MiniLM-L6-v2")
 
-persistent_directory="db/chroma_db"
+persistent_directory = "db/chroma_db"
 
-db=Chroma(
-    persist_directory=persistent_directory,
-    embedding_function=embedding_model,
-    collection_metadata={"hnsw:space": "cosine"}
-)
+db = Chroma(
+        persist_directory=persistent_directory,
+        embedding_function=embedding_model,
+        collection_metadata={"hnsw:space": "cosine"}
+    )
 
-query = "how mush meta worth?"
+model = ChatGroq(model="openai/gpt-oss-20b")
 
-# This will return highest top 3 similarity chunks
-retriever=db.as_retriever(search_kwargs={"k":3})
+chat_history=[]
 
-retriever_result=retriever.invoke(query)
+def ask_questions(query):
 
-print(f"User query :{query}")
+    print(f"User query :{query}")
 
-for i,doc in enumerate(retriever_result,1):
-    print(f"Document {i}:\n{doc.page_content}\n")
+    if chat_history:
+# Tells model to give the new question appropriately,(ex:-"what is the networth of it"=>what is the networth of meta")
+        messages=[
+            SystemMessage(content="Given the chat history, rewrite the new question to be standalone and searchable. Just return the rewritten question."),
+        ] + chat_history + [
+            HumanMessage(content=f"New question :{query}")
+        ]
+
+        result=model.invoke(messages)
+        user_question=result.content.strip()
+        print(f"User question:{user_question}")
+
+    else:
+        user_question=query
+
+    # This will return highest top 3 similarity chunks
+    retriever = db.as_retriever(search_kwargs={"k": 3})
+
+    retriever_result = retriever.invoke(user_question)
+
+    for i,doc in enumerate(retriever_result,1):
+        print(f"Document {i}:\n{doc.page_content}\n")
 
 
-combined_input=f"""Based on the following documents, please answer this question:{query}
+    combined_input=f"""Based on the following documents, please answer this question:{query}
+    Documents:
+    {chr(10).join([f"-{doc.page_content}" for doc in retriever_result])}
+    Please provide a clear, helpful answer using only the information from these documents. If you can't find the answer in the documents, say "I don't have enough information to answer that question based on the provided documents."
+    """""
 
-Documents:
-{chr(10).join([f"-{doc.page_content}" for doc in retriever_result])}
+    messages=[
+        SystemMessage(content="You are a helpful assistant."),
+        HumanMessage(content=combined_input)
+    ]
 
-Please provide a clear, helpful answer using only the information from these documents. If you can't find the answer in the documents, say "I don't have enough information to answer that question based on the provided documents."
-"""""
+    result=model.invoke(messages)
+    answer=result.content
 
-model=ChatGroq(model="openai/gpt-oss-20b")
+    chat_history.append(HumanMessage(content=query))
+    chat_history.append(AIMessage(content=answer))
 
-messages=[
-    SystemMessage(content="You are a helpful assistant."),
-    HumanMessage(content=combined_input)
-]
+    # Display the  result
+    print("\n--- Generated Response ---")
 
-result=model.invoke(messages)
+    print("Content only:")
+    print(answer)
 
-# Display the  result
-print("\n--- Generated Response ---")
+    return answer
 
-print("Content only:")
-print(result.content)
+if __name__ == "__main__":
+
+    while True:
+        question=input("Ask questions (q to quit):")
+
+        if question.lower() =="q":
+            break
+
+        ask_questions(query=question)
